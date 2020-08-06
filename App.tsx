3bfbcc83d0dfafb32d9e10ce68833301483e2d27
stephbsb <stephany0303@gmail.com>
2020-08-06 11:17:44 -0300

    Adicionando Landing Page no App

--- App.tsx
+++ App.tsx
@@ -2,13 +2,38 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Landing from './src/pages/Landing';
+import { AppLoading } from 'expo'; //mostrar splashscreen ate app estar carregado - fonts
+
+import {
+  Archivo_400Regular,
+  Archivo_700Bold,
+  useFonts,
+} from '@expo-google-fonts/archivo';
+
+import {
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+} from '@expo-google-fonts/poppins';
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <>
+        <Landing />
+        <StatusBar style='light' />
+      </>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
